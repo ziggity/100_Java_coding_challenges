57482dd29d78c4b47a5cbaf80a7c362f5129a6a6
Zach Beecher <zachbeecher@gmail.com>
2020-09-07 13:38:38 -0700

    added a two algos

---
+++ src/oneHundredProblems/AddItAll.java
@@ -0,0 +1,43 @@
+package oneHundredProblems;
+//Write a function that returns the sum of two numbers.
+//Write a function that returns the sum of all numbers regardless of # of params.
+//Example
+//
+//For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+//
+//Hints
+//
+//Arithmetic Operators
+//Rest Operator
+//forEach()
+//Input/Output
+//
+//[time limit] 4000ms (js)
+//[input] integer param1
+//Guaranteed constraints:
+//
+//-100 ≤ param1 ≤ 1000.
+//
+//[input] integer param2
+//
+//Guaranteed constraints: -100 ≤ param2 ≤ 1000.
+//
+//[output] integer
+//
+//The sum of the two inputs.
+public class AddItAll {
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5}; // should return 15;
+		System.out.println( addItUp(a)); 	
+	}
+
+	private static int addItUp(int[] a) {
+		int counter = 0;
+		for(int i : a) {
+			counter += i;
+		}
+		return counter;
+	}
+	
+}
