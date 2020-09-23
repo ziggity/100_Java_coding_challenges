ce16dbb88ef2df0bb623112b1bced03f6d803697
Zach Beecher <zachbeecher@gmail.com>
2020-09-22 20:57:22 -0700

    added code to  palindrome lowercase

--- src/oneHundredProblems/CaseInsensitve.java
+++ src/oneHundredProblems/CaseInsensitve.java
@@ -29,8 +29,23 @@ Non-empty string consisting of English letters. Guaranteed constraints: 4 ≤ in
 public class CaseInsensitve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input1 = "AbAbAa";
+		System.out.println(caseInsensitive(input1));
 	}
 
-}
+	private static boolean caseInsensitive(String text) {
+
+		    String clean = text.replaceAll("\\s+", "").toLowerCase();
+		    int length = clean.length();
+		    int forward = 0;
+		    int backward = length - 1;
+		    while (backward > forward) {
+		        char forwardChar = clean.charAt(forward++);
+		        char backwardChar = clean.charAt(backward--);
+		        if (forwardChar != backwardChar)
+		            return false;
+		    }
+		    return true;
+		}
+	}
+
