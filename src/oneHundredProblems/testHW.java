e64cd155f5be60cdc1de87fb29f0bb973f67406f
Zach Beecher <zachbeecher@gmail.com>
2020-09-29 20:32:00 -0700

    Added companyBot - calculate nested array values at 0 index, and loop through, checking if 1st index is 1 if not if its 0 add value to time, if its 1 add it to correctness. Divide at end and return double result.

--- src/oneHundredProblems/testHW.java
+++ src/oneHundredProblems/testHW.java
@@ -7,27 +7,11 @@ import java.util.Arrays;
 public class testHW{
 
 	public static void main(String[] args) {
-		
-		public List createListWithEveryOtherElement(List list) {
-
-	        List<String> names = Arrays.asList("Rhonda", "Marissa", "Alexis", "Devon", "Adrian", "Tiana");
-	        List<String> everyOtherName = everyOtherElement(names);
-	        System.out.println(everyOtherName);
-	        System.out.println(names.get(4));
-	    }
-
-	    private static List<String> everyOtherElement(List<String> names) {
-	        List<String> list = new ArrayList<String>();
-
-	        for(int index = 0; index < names.size(); index += 2) {
-	            list.add(names.get(index));
-	        }
-	        return list;
-	    }
-
-					
-
+		int number2 = 1;
+		for (int num = 1; num <=100; num ++){
+		while (number2 >= 0 || number2 <=99) {
+		System.out.println(number2);
+		number2 = number2 + 2;
 		}
-}
-
-	}
+		}
+}}
