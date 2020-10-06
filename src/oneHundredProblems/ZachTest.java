48ce9ce6f90e87fee1c2ed14e4b1433988577f08
Zach Beecher <zachbeecher@gmail.com>
2020-10-05 18:45:22 -0700

    added

--- src/oneHundredProblems/ZachTest.java
+++ src/oneHundredProblems/ZachTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 
@@ -16,64 +17,14 @@ public static void main(String[] args) {
 
 
 
-    List<String> list = Arrays.asList("Hello", "World!", "hi", "13301", "11010");
+int i = 1;
 
-
-
-System.out.print(createListWithEveryOtherElement(list));
-
-}
-
-     public static  List createListWithEveryOtherElement(List list) {
-
-        List<String> resultList = new ArrayList<String>();
-
-         for (int i = 0; i < list.size(); i= i+2) {
-
-         
-
-         //System.out.println(list.get(i));
-
-         resultList.add((String) list.get(i));
-
-         
-
-         }
-
-         
-
-         
-
-System.out.println("0" + resultList.get(0));
-
-System.out.println("1" + resultList.get(1));
-
-System.out.println("2" + resultList.get(2));
-
-
-
-//System.out.println(resultList.remove(resultList.size()-1));
-
-if(resultList.size() == list.size()/2 ) {
-
-return  resultList;
-
-}
-
-else {
-
-resultList.remove(resultList.size()-1);
-
-}  
-
-    
-
-return  resultList;
-
-
-
+for (i = 1; i < 100; i = i + 2) {
+    System.out.println(i);
     }
 
 
 
-}
+
+
+}}
