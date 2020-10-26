f7e6367ccf328c0f1b9104c95419012e88725c42
Zach Beecher <zachbeecher@gmail.com>
2020-10-25 22:41:29 -0700

    Test

--- src/oneHundredProblems/MinRewards.java
+++ src/oneHundredProblems/MinRewards.java
@@ -43,7 +43,7 @@ class MinRewards {
 		System.out.println();
 	}
 
-	//Approach 2: LocalMin Approach
+	//Approach 2: LocalMin Approach here
 
 	static ArrayList<Integer> localMin(int []a, int n) {
 		ArrayList<Integer> list = new ArrayList<>();
